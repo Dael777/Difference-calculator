a752a864d7759a49bebe30aaa38331db8782e1e6
Dael777 <dael85@gmail.com>
2019-02-13 00:26:35 +0300

    tests fixes 2

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -7,24 +7,19 @@ test('no args', () => {
 
 const data2 = fs.readFileSync('./__tests__/__fixtures__/2.txt', 'utf-8');
 test('empty first file', () => {
-  expect(gendiff('/__tests__/__fixtures__/empty.json', '/home/dael777/Public/Lessons/js/gendiff/__tests__/__fixtures__/after.json')).toBe(data2);
+  expect(gendiff('/__tests__/__fixtures__/empty.json', '/__tests__/__fixtures__/after.json')).toBe(data2);
 });
 
 const data3 = fs.readFileSync('./__tests__/__fixtures__/3.txt', 'utf-8');
 test('empty second file', () => {
-  expect(gendiff('/home/dael777/Public/Lessons/js/gendiff/__tests__/__fixtures__/before.json', '/__tests__/__fixtures__/empty.json')).toBe(data3);
+  expect(gendiff('/__tests__/__fixtures__/before.json', '/__tests__/__fixtures__/empty.json')).toBe(data3);
 });
 
 const data4 = fs.readFileSync('./__tests__/__fixtures__/4.txt', 'utf-8');
-test('absolute paths', () => {
-  expect(gendiff('/home/dael777/Public/Lessons/js/gendiff/__tests__/__fixtures__/before.json', '/home/dael777/Public/Lessons/js/gendiff/__tests__/__fixtures__/after.json')).toBe(data4);
-});
-
-const data5 = fs.readFileSync('./__tests__/__fixtures__/5.txt', 'utf-8');
 test('relative paths', () => {
-  expect(gendiff('/__tests__/__fixtures__/before.json', '/__tests__/__fixtures__/after.json')).toBe(data5);
+  expect(gendiff('/__tests__/__fixtures__/before.json', '/__tests__/__fixtures__/after.json')).toBe(data4);
 });
 
 test('relative paths without slash', () => {
-  expect(gendiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')).toBe(data5);
+  expect(gendiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')).toBe(data4);
 });
