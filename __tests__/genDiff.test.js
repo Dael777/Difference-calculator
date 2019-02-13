728ec87bb68363b3391aee6ef17594ad274c2ba7
Dael777 <dael85@gmail.com>
2019-02-13 15:05:17 +0300

    step 2 fixes

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,25 +1,23 @@
 import fs from 'fs';
 import gendiff from '../src';
 
-test('no args', () => {
-  expect(gendiff('/__tests__/__fixtures__/empty.json')).toBeFalsy();
-});
+describe('Compare Files', () => {
+  const data2 = fs.readFileSync('./__tests__/__fixtures__/2.txt', 'utf-8');
+  it('empty first file', () => {
+    expect(gendiff('/__tests__/__fixtures__/empty.json', '/__tests__/__fixtures__/after.json')).toBe(data2);
+  });
 
-const data2 = fs.readFileSync('./__tests__/__fixtures__/2.txt', 'utf-8');
-test('empty first file', () => {
-  expect(gendiff('/__tests__/__fixtures__/empty.json', '/__tests__/__fixtures__/after.json')).toBe(data2);
-});
+  const data3 = fs.readFileSync('./__tests__/__fixtures__/3.txt', 'utf-8');
+  it('empty second file', () => {
+    expect(gendiff('/__tests__/__fixtures__/before.json', '/__tests__/__fixtures__/empty.json')).toBe(data3);
+  });
 
-const data3 = fs.readFileSync('./__tests__/__fixtures__/3.txt', 'utf-8');
-test('empty second file', () => {
-  expect(gendiff('/__tests__/__fixtures__/before.json', '/__tests__/__fixtures__/empty.json')).toBe(data3);
-});
+  const data4 = fs.readFileSync('./__tests__/__fixtures__/4.txt', 'utf-8');
+  it('relative paths', () => {
+    expect(gendiff('/__tests__/__fixtures__/before.json', '/__tests__/__fixtures__/after.json')).toBe(data4);
+  });
 
-const data4 = fs.readFileSync('./__tests__/__fixtures__/4.txt', 'utf-8');
-test('relative paths', () => {
-  expect(gendiff('/__tests__/__fixtures__/before.json', '/__tests__/__fixtures__/after.json')).toBe(data4);
-});
-
-test('relative paths without slash', () => {
-  expect(gendiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')).toBe(data4);
+  it('relative paths without slash', () => {
+    expect(gendiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')).toBe(data4);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -6,34 +6,35 @@ const currentPath = process.cwd();
 
 const checkPath = filePath => ((filePath.includes(currentPath)) ? filePath : `${currentPath}${filePath}`);
 
-export default (pathToFile1, pathToFile2) => {
-  if (!pathToFile1 || !pathToFile2) return false;
+const compareFiles = (firstFile, secondFile) => {
+  const allKeys = _.union(Object.keys(firstFile), Object.keys(secondFile));
+  const filesDifference = allKeys.map((elem) => {
+    if (_.has(firstFile, elem)) {
+      if (_.has(secondFile, elem)) {
+        return (firstFile[elem] === secondFile[elem])
+          ? `    ${elem}: ${firstFile[elem]}\n`
+          : `  + ${elem}: ${secondFile[elem]}\n  - ${elem}: ${firstFile[elem]}\n`;
+      }
+      return `  - ${elem}: ${firstFile[elem]}\n`;
+    }
+    return `  + ${elem}: ${secondFile[elem]}\n`;
+  });
+  return `{\n${filesDifference.join('')}}`;
+};
 
+const getJsonContent = fullPath => JSON.parse(fs.readFileSync(fullPath));
+
+export default (pathToFile1, pathToFile2) => {
   const fullPathToFile1 = checkPath(path.normalize(`/${pathToFile1}`));
   const fullPathToFile2 = checkPath(path.normalize(`/${pathToFile2}`));
 
-  const compareFiles = (firstFile, secondFile) => {
-    const firstFileCheck = Object.keys(firstFile).reduce((acc, elem) => {
-      if (_.has(secondFile, elem)) {
-        return (firstFile[elem] === secondFile[elem])
-          ? `${acc}    ${elem}: ${firstFile[elem]}\n`
-          : `${acc}  + ${elem}: ${secondFile[elem]}\n  - ${elem}: ${firstFile[elem]}\n`;
-      }
-      return `${acc}  - ${elem}: ${firstFile[elem]}\n`;
-    }, '');
-
-    const secondFileCheck = Object.keys(secondFile).reduce((acc, elem) => ((!_.has(firstFile, elem)) ? `${acc}  + ${elem}: ${secondFile[elem]}\n` : acc), firstFileCheck);
-
-    return secondFileCheck;
-  };
-
   if (fs.statSync(fullPathToFile1).size === 0) {
-    return `{\n${compareFiles({}, JSON.parse(fs.readFileSync(fullPathToFile2)))}}`;
+    return compareFiles({}, getJsonContent(fullPathToFile2));
   }
 
   if (fs.statSync(fullPathToFile2).size === 0) {
-    return `{\n${compareFiles(JSON.parse(fs.readFileSync(fullPathToFile1)), {})}}`;
+    return compareFiles(getJsonContent(fullPathToFile1), {});
   }
 
-  return `{\n${compareFiles(JSON.parse(fs.readFileSync(fullPathToFile1)), JSON.parse(fs.readFileSync(fullPathToFile2)))}}`;
+  return compareFiles(getJsonContent(fullPathToFile1), getJsonContent(fullPathToFile2));
 };
